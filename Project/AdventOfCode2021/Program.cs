63bdf4a1af8d87b36fe60f259ed334d448085374
Silas <SilasTheBaker@gmail.com>
2021-12-07 00:01:28 -0800

    Add Day 7

--- Project/AdventOfCode2021/Program.cs
+++ Project/AdventOfCode2021/Program.cs
@@ -75,6 +75,7 @@ namespace AdventOfCode2021
                 new Day4(),
                 new Day5(),
                 new Day6(),
+                new Day7(),
             };
 
             if (day < 0)
